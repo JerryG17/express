914830fc8adc35b9ac23584c050ab18be48e3fe3
gerardog17 <pacoescobedo17@gmail.com>
2018-07-18 11:23:58 -0500

    cambios

---
+++ node-api/models/users.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const UserSchema = mongoose.Schema({
+    username: String,
+    first_name: String,
+    last_name: String,
+});
+
+const User = mongoose.model('User','UserSchema')
+module.exports = User;
